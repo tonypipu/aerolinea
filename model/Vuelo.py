4bc55b4b4f0809f045fe2d60608405969f6f2354
Tony <50093832+tonypipu@users.noreply.github.com>
2022-09-30 07:28:13 -0500

    actualizacion carpeta model
    se actulizo la carpeta model

---
+++ model/Vuelo.py
@@ -0,0 +1,12 @@
+
+
+
+class Vuelo(object):
+    def __init__(self,ruta,avion,salida):
+        
+        self.ruta:str=ruta
+        self.avion:str=avion
+        self.salida:str=salida
+       
+    
+        
