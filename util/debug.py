a6cd14ad4fef30a33c444b0d4c4b52d8fd3aa190
Tony <50093832+tonypipu@users.noreply.github.com>
2022-09-30 07:31:51 -0500

    Actualizacion de la los utils
    archivos con funciones utiles para el proyecto

---
+++ util/debug.py
@@ -0,0 +1,17 @@
+import logging 
+logging.basicConfig( level=logging.DEBUG, filename='/home/ubuntu/aerolinea/debug.log',format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+
+logger = logging.getLogger('Example_Log')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('debug.log')
+fh.setLevel(logging.DEBUG)
+
+
+### comandos para el log
+
+# logger.debug('Debug Message')
+# logger.info('Info Message')
+# logger.warning('Warning')
+# logger.error('An Error Occured')
+# logger.critical('Critical error shutdown')
